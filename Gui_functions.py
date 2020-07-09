d9970b11832eb400fe5d385845ad02093260a50f
UntriexTv <untriex.programming@gmail.com>
2020-07-09 12:03:47 +0200

    Added library to easily make buttons, text etc

---
+++ Gui_functions.py
@@ -0,0 +1,40 @@
+import pygame
+
+red = (200,0,0)
+green = (0,200,0)
+bright_red = (255,0,0)
+bright_green = (0,255,0)
+black = (0,0,0)
+white = (255,255,255)
+
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+
+def message_display(display, text, x, y, height, font="comicsansms"):
+    largeText = pygame.font.SysFont(font,height)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = ((x),(y))
+    display.blit(TextSurf, TextRect)
+
+
+def button(display, msg,x,y,w,h,ic,ac,action=None, font="comicsansms", height = 20):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(display, ac,(x,y,w,h))
+
+        if click[0] == 1 and action != None:
+            action()
+    else:
+        pygame.draw.rect(display, ic,(x,y,w,h))
+
+    try:
+        smallText = pygame.font.SysFont(font,height)
+        textSurf, textRect = text_objects(msg, smallText)
+        textRect.center = ( (x+(w/2)), (y+(h/2)) )
+        display.blit(textSurf, textRect)
+    except:
+        pass
