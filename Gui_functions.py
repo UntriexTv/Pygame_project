aed30fbe172896d74bd97f3f80ec9a324f11ebdb
UntriexTv <untriex.programming@gmail.com>
2020-07-09 19:42:37 +0200

    some updates for easier work

--- Gui_functions.py
+++ Gui_functions.py
@@ -8,19 +8,19 @@ black = (0,0,0)
 white = (255,255,255)
 
 
-def text_objects(text, font):
-    textSurface = font.render(text, True, black)
+def text_objects(text, font, color=black):
+    textSurface = font.render(text, True, color)
     return textSurface, textSurface.get_rect()
 
 
-def message_display(display, text, x, y, height, font="comicsansms"):
+def message_display(display, text, x, y, height, font="comicsansms", color=black):
     largeText = pygame.font.SysFont(font,height)
-    TextSurf, TextRect = text_objects(text, largeText)
+    TextSurf, TextRect = text_objects(text, largeText, color)
     TextRect.center = ((x),(y))
     display.blit(TextSurf, TextRect)
 
 
-def button(display, msg,x,y,w,h,ic,ac,action=None, font="comicsansms", height = 20):
+def button(display, msg,x,y,w,h,ic,ac,action=None, font="comicsansms", height=20):
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
     if x+w > mouse[0] > x and y+h > mouse[1] > y:
